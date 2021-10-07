d6f3795b66b6baa2933f3975036dea83608a2592
Eliska Talianova <xtalia00@vut.cz>
2021-10-07 13:23:14 +0200

    provedena změna

--- vracim_tal.R
+++ vracim_tal.R
@@ -1,16 +1,22 @@
 vraceni_minci = function(M,c,d){
-  vraceni={}
-  for (i in c(1:(M))) {
-    hodnotamince = c(50,20,10,5,2,1)
-      if (hodnotamince==M){
-        vraceni=c(vraceni,hodnotamince)
-      }
-  }
-}
-#   while (M>0) {
-#     mince = c(50,20,10,5,2,1)
-#     if (M>50){
-#       
-#     }
-#   }
+  vraceni=c()
+  # for (i in c(1:(M))) {
+  #   hodnotamince = c(50,20,10,5,2,1)
+  #     if (hodnotamince==M){
+  #       vraceni=c(vraceni,hodnotamince)
+  #     }
+  # }
 # }
+   while (M>0) {
+     mince = c(50,20,10,5,2,1)
+     for (i in 1:length(mince)) {
+       x = floor(M/mince(i))
+       if (x>0){
+         vraceni=c(vraceni,mince(i))
+         M=M-mince(i)
+       }
+     }
+      
+     }
+   }
+
