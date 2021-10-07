848b93ee333260ecaefc04b50c4af63c4b4d7cf0
Eliska Talianova <xtalia00@vut.cz>
2021-10-07 13:12:28 +0200

    Vraceni mincí ani náhodou.

---
+++ vracim_tal.R
@@ -0,0 +1,16 @@
+vraceni_minci = function(M,c,d){
+  vraceni={}
+  for (i in c(1:(M))) {
+    hodnotamince = c(50,20,10,5,2,1)
+      if (hodnotamince==M){
+        vraceni=c(vraceni,hodnotamince)
+      }
+  }
+}
+#   while (M>0) {
+#     mince = c(50,20,10,5,2,1)
+#     if (M>50){
+#       
+#     }
+#   }
+# }
