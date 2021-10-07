1ff62348defdaad1391e83fbf7b4a86704ec5907
Eliska Talianova <xtalia00@vut.cz>
2021-10-07 14:25:18 +0200

    přidána iterace

--- cokoladojdu_tal.R
+++ cokoladojdu_tal.R
@@ -13,3 +13,27 @@ cokoladojdu = function(M,r,s){
   }
   return(coko)
 }
+
+
+
+
+
+
+
+slozitacesta = function(M){
+  
+  size=dim(M)
+  K1=seq(from=size[1]-1, to=1, by=-1)
+  
+for (i in K1) {
+  K2=seq(from=i, to=1, by=-1)
+  for (j in K2) {
+    Cdolu = M[i+1,j]+M[i,j]
+    Csikmo = M[i+1, j+1]+M[i,j]
+    M[i,j] = max(Cdolu,Csikmo)
+  }
+     
+    }
+    
+  return(M[1,1])
+}
